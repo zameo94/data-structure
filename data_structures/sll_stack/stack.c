f73757601c6a61628e95ce84c61b66cc258c03ac
matteo <matdeliperi.md@gmail.com>
2026-01-18 17:15:47 +0100

    added stack_size, stack_clear and stack_duplicate

--- data_structures/sll_stack/stack.c
+++ data_structures/sll_stack/stack.c
@@ -87,17 +87,55 @@ int stack_peek(sll_stack *stack, int *output) {
     return get_head(stack->list, output);
 }
 
+/* SIZE */
+
+int stack_size(sll_stack *stack) {
+    if(stack == NULL || stack->list == NULL) return SLL_ERROR_LIST_NOT_ALLOCATED;
+
+    return stack->list->length;
+}
+
+/* CLEAR */
+
+int stack_clear(sll_stack *stack) {
+    if(stack == NULL || stack->list == NULL) return SLL_ERROR_LIST_NOT_ALLOCATED;
+
+    return clear_list(stack->list);
+}
+
+/* DUPLICATE */
+
+int stack_duplicate(sll_stack *old_stack, sll_stack *new_stack) {
+    if(old_stack == NULL || old_stack->list == NULL) return SLL_ERROR_LIST_NOT_ALLOCATED;
+    if(new_stack == NULL || new_stack->list == NULL) return SLL_ERROR_LIST_NOT_ALLOCATED;
+
+    return duplicate_list(old_stack->list, new_stack->list);
+}
+
 int main(void) {
     sll_stack *stack = stack_init();
     stack_push(stack, 10);
     stack_push(stack, 20);
 
+    printf("Stack size: %d\n", stack_size(stack));
+    printf("Stack:\n");
     stack_print(stack);
+
+    sll_stack *stack2 = stack_init();
+    printf("Duplication....\n");
+    stack_duplicate(stack, stack2);
+    printf("Stack 2:\n");
+    stack_print(stack2);
     
     int val;
     stack_pop(stack, &val);
     printf("Popped: %d\n", val);
 
+    printf("Stack size: %d\n", stack_size(stack));
+    stack_print(stack);
+
+    stack_clear(stack);
+    printf("Stack size: %d\n", stack_size(stack));
     stack_print(stack);
     
     stack_free(&stack);
--- data_structures/sll_stack/stack.h
+++ data_structures/sll_stack/stack.h
@@ -25,5 +25,8 @@ int stack_peek(sll_stack *stack, int *output);
 void stack_print(sll_stack *stack);
 int stack_free(sll_stack **stack_ref);
 bool stack_is_empty(sll_stack *stack);
+int stack_size(sll_stack *stack);
+int stack_clear(sll_stack *stack);
+int stack_duplicate(sll_stack *old_stack, sll_stack *new_stack);
 
 #endif
